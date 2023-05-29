223d19b400e7001909a7993fed929862410b48ec
RafaelMunizz <rafaelvictormuniz@gmail.com>
2023-05-29 17:37:51 -0300

    Add hookins

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -2,11 +2,18 @@ import './CampoTexto.css'
 
 // Componentes React, por padrão, devem começar com letras maiúsculas
 const CampoTexto = (props) => {
-    // O retorno () geralmente é algo padrão que será utilizado
+
+    const placeholderModificada = `${props.placeholder}...`;
+
+    const aoDigitado = (evento) => {
+        props.aoAlterado(evento.target.value);
+    }
+
+    // O return () geralmente é algo padrão que será utilizado
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={props.placeholder}/>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
         </div>
     )
 }
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,11 +1,12 @@
-import './Formulario.css'
-import CampoTexto from '../CampoTexto'
-import ListaSuspensa from '../ListaSuspensa'
-import Botao from '../Botao'
+import './Formulario.css';
+import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
+import { useState } from 'react';
+import Botao from '../Botao';
 
 const Formulario = () => {
 
-    const itens = [
+    const times = [
         'Programação',
         'Front-End',
         'Data Science',
@@ -13,21 +14,26 @@ const Formulario = () => {
         'UX e Design',
         'Mobile', 
         'Inovação e Gestão'
-    ]
+    ];
+
+    const [nome, setNome] = useState('');
+    const [cargo, setCargo] = useState('');
+    const [imagem, setImagem] = useState(''); 
 
     function aoSalvar(evento) {
-        evento.preventDefault();
-        console.log('Formulário submetido!')
+        evento.preventDefault(); // Impedir que a página atualize após enviar o formulário
+        console.log('Formulário submetido!');
+        console.log(nome, cargo, imagem);
     }
 
     return (
         <section className='formulario'>
             <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto obrigatorio={true} label='Nome' placeholder='Digite o seu nome'/>
-                <CampoTexto obrigatorio={true} label='Cargo' placeholder='Digite o seu cargo'/>
-                <CampoTexto label='Imagem' placeholder='Digite o endereço da imagem'/>
-                <ListaSuspensa obrigatorio={true} itens={itens} label='Time'/>
+                <CampoTexto obrigatorio={true} label='Nome' placeholder='Digite o seu nome' valor={nome} aoAlterado={valor => setNome(valor)}/>
+                <CampoTexto obrigatorio={true} label='Cargo' placeholder='Digite o seu cargo' valor={cargo} aoAlterado={valor => setCargo(valor)}/>
+                <CampoTexto label='Imagem' placeholder='Digite o endereço da imagem' valor={imagem} aoAlterado={valor => setImagem(valor)}/>
+                <ListaSuspensa obrigatorio={true} itens={times} label='Time'/>
                 <Botao>
                     Criar card    
                 </Botao>
