bcc9707863061fce2735ba99c2aa7e7c262441b7
RafaelMunizz <rafaelvictormuniz@gmail.com>
2023-06-06 17:52:44 -0300

    updates

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -3,7 +3,7 @@ import './Time.css'
 const Time = (props) => {
     // HTML que será do componente
     return (
-        <section className='time'>
+        <section className='time' style={{backgroundColor: props.corSecundaria}}>
             <h3>{props.nome}</h3>
         </section>
     )
